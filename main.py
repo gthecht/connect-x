bc4a5d00fba348625fcbc30bfb7f4f1acfcf36cc
Doraz1 <62758770+Doraz1@users.noreply.github.com>
2020-04-01 04:32:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
+import os
 import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 # region Constants
 ELITISM = 1
@@ -13,131 +18,38 @@ RECEPTIVE_FIELD_DEFAULT = 2
 # endregion
 
 
-class Net:
-    # Todo: add compatibility with varying number of image channels
-    # Todo: implement the full net backpropagation
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 5, 3)  # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(5, 10, 3)
 
-    def __init__(self, identification, inputs=np.array([])):
-        self.identification = identification
-        self.fitness = np.random.randint(10)
+        x = torch.rand(16, 16).view(-1, 1, 16, 16)  # reshape so that height and width are (16,16) with changing number of channels
+        self.fc_input_shape = None
 
-        self.inputs = np.array([])  # say this is a 5x5 image
-        self.outputs = np.array([])
-        self.layer_dict = {}
+        self.convs(x)
 
-    def __str__(self):
-        id_str = f'--Net--\nNet id: {self.identification}\n'
-        fitness_str = f'Fitness: {self.fitness}\n'
+        self.fc1 = nn.Linear(self.fc_input_shape, 20)
+        self.fc2 = nn.Linear(20, 8)
 
-        return id_str + fitness_str
+    def convs(self, x):
+        # this forwards an input through the convolutional layers
+        # And finds fc_input_shape = final convolutional layer output size
 
-    def add_layer(self, layer_num):
-        my_conv_layer = ConvolutionLayer(layer_num)
-        self.layer_dict[my_conv_layer.layer_number] = my_conv_layer
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
 
-    def activate(self, net_input):
-        layers_dict = self.layer_dict.values()
-        prev_input = net_input
+        if self.fc_input_shape is None:
+            self.fc_input_shape = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
 
-        for layer in layers_dict:
-            layer.set_inputs(prev_input)
-            layer.calc_output()  # update the layer output
+        return x
 
-            prev_input = layer.outputs  # update the input for the next layer
-
-        final_layer = list(layers_dict)[-1]
-        output = final_layer.outputs
-
-        return output
-
-    def backpropagate(self):
-        # dX, dW = layer.backpropagate
-
-        pass
-
-
-class ConvolutionLayer:
-    def __init__(self, layer_number, f=RECEPTIVE_FIELD_DEFAULT, k=1):
-        self.layer_number = layer_number
-        self.weights = np.random.randint(-1, 1, (f, f))
-        self.f = f
-        self.k = k
-
-        self.input = np.array([])
-        self.outputs = np.array([])
-
-    def set_inputs(self, input_vec):
-        self.input = input_vec
-
-    def calc_output(self):
-        # Calculating the convolution layer output given the input, kernel and kernel number
-        in_rows, in_cols = self.input.shape
-        f = self.f
-        kernel = self.weights
-
-        out_rows = in_rows - f + 1
-        out_cols = in_cols - f + 1
-
-        outputs = np.zeros((out_rows, out_cols))
-
-        for row in range(out_rows):
-            for col in range(out_cols):
-                convolved = self.input[row:row + f, col:col + f] * kernel
-                outputs[row, col] = np.sum(convolved)
-
-        self.outputs = outputs
-
-    def calc_gradients(self, dY):
-        # Calculating the gradients of the layer given the loss gradient wrt output (dY)
-
-        # Retrieving the input (X) and the kernel weights (W)
-        X = self.input
-        W = self.weights
-
-        # Retrieving dimensions from W's shape
-        (f, f) = W.shape
-
-        # Retrieving dimensions from dY's shape
-        (rows_Y, cols_Y) = dY.shape
-
-        # Initializing dX, dW with the correct shapes
-        dX = np.zeros(X.shape)
-        dW = np.zeros(W.shape)
-
-        # Looping over vertical(h) and horizontal(w) axis of the output
-        for h in range(rows_Y):
-            for w in range(cols_Y):
-                dX[h:h + f, w:w + f] += dY[h, w] * W
-                dW += dY[h, w] * X[h:h + f, w:w + f]
-
-        return dX, dW
-
-    def backpropagate(self):
-        # Calculating the layer gradients:
-        # from loss function to input (dX)
-        # and from loss function to weights (dW)
-
-        in_rows, in_cols = self.input.shape
-        out_rows = in_rows - self.f + 1
-
-        dY = np.ones((out_rows, out_rows))  # error vector wrt output loss (Y)
-
-        dX, dW = self.calc_gradients(dY)
-
-        return dX, dW
-
-    def __str__(self):
-        id_str = f'--Convolution layer--\nLayer number: {self.layer_number}\n'
-        input_str = f'Input matrix:\n'
-        for elem in self.input:
-            input_str += f'\t{elem}\n'
-        weight_str = f'Weight kernel:\n'
-        for elem in self.weights:
-            weight_str += f'\t{elem}\n'
-        output_str = f'Output:\n'
-        for elem in self.outputs:
-            output_str += f'\t{elem}\n'
-        return id_str + input_str + weight_str + output_str
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self.fc_input_shape)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
 
 
 class EvolutionEngine:
@@ -171,13 +83,6 @@ class EvolutionEngine:
 
 
 if __name__ == '__main__':
-
-
-    # print(mynet)
-    # print(mynet.activate())
-    mynet = Net(1)
-    mynet.add_layer(1) # adds a convolution layer as the first layer of the net
-
-    in_vec = np.random.randint(-1, 1, (3, 3))
-    net_out = mynet.activate(in_vec)
-    print(net_out)
+    net = Net()
+    print(net)
+    # in_vec = np.random.randint(-1, 1, (4, 4))
