cfe2df05e3f37fade080a2aa6ec736629e4192e2
giladh <gdpstd@gmail.com>
2020-04-11 13:41:05 +0300

    Changed board class so that it uses score function we can change

--- code/board.py
+++ code/board.py
@@ -5,19 +5,25 @@ from numpy import random as rand
 from kaggle_environments import evaluate, make, utils
 
 class Board:
-  def __init__(self, config):
+  def __init__(self, config, scoreFunc = False):
     self.config = config
     self.timeout = config["timeout"]
     self.columns = config["columns"]
     self.rows = config["rows"]
     self.inarow = config["inarow"]
     self.steps = config["steps"]
-    self.SCOREPOWER = 3
+    if (scoreFunc):
+      self.scoreFunc = scoreFunc
+    else:
+      self.scoreFunc = self.defaultScoreFunc
 
-  def score(self, outcomeDict, openBelow):
+  # I think I might make the scorFunc more general, and move the decision to where I call this function.
+  @staticmethod
+  def defaultScoreFunc(outcomeDict, openBelow):
+    return sum(np.sum((outcome > 0) * np.power(outcome, 3) / (openBelow[name] + 1)) for name, outcome in outcomeDict.items())
+    # Options for scoreFunc:
     # return (max(np.amax(outcome) for name, outcome in outcomeDict.items()) / self.inarow)  ** self.SCOREPOWER
     # return sum(np.sum((outcome > 0) * np.power(outcome, self.SCOREPOWER)) for name, outcome in outcomeDict.items())
-    return sum(np.sum((outcome > 0) * np.power(outcome, self.SCOREPOWER) / (openBelow[name] + 1)) for name, outcome in outcomeDict.items())
 
   def checkState(self, myBoard, opponentBoard, openBoard):
     gameFilters = { 
@@ -40,8 +46,7 @@ class Board:
     openBelow = { name: np.flipud(convolve(1 * (openBoard), filter, mode="constant", cval=0, origin=[-int((self.rows ) / 2), \
                 int((filter.shape[1] - 1) / 2)])) for name, filter in openBelowFilters.items() }
     openBelow = { name: matrix[0:filtersOutCome[name].shape[0], 0:filtersOutCome[name].shape[1]] for name, matrix in openBelow.items() }
-    score = self.score(filtersOutCome, openBelow)
-    return { "score": score,  "filtersOutCome": filtersOutCome }
+    return { "filtersOutCome": filtersOutCome, "openBelow": openBelow }
 
   def playOptions(self, openBoard):
     playOptionsFilter = np.array([[-1], [1]])
@@ -55,9 +60,15 @@ class Board:
     opponentBoard = (self.board == 3 - observation["mark"]) # so that if I'm 2 then they are 1 and the opposite
     openBoard = (self.board == 0)
     playOptions = self.playOptions(openBoard)
+
     myState = self.checkState(myBoard, opponentBoard, openBoard)
+    myScore = self.scoreFunc(myState["filtersOutCome"], myState["openBelow"])
+    
     opponentState = self.checkState(opponentBoard, myBoard, openBoard)
-    score = myState["score"] - opponentState["score"]
+    opponentScore = self.scoreFunc(opponentState["filtersOutCome"], opponentState["openBelow"])
+    
+    score = myScore - opponentScore
+
     self.boardState = { 
       "score": score,
       "myBoard": myBoard,
@@ -68,39 +79,40 @@ class Board:
     }
     return self.boardState
 
-def testBoard(printStates):
-  observation = { 
-    'board': [0, 0, 0, 0, 0, 0, 0,
-              0, 1, 0, 0, 0, 0, 0,
-              0, 2, 0, 0, 0, 0, 0,
-              0, 1, 0, 2, 2, 1, 0,
-              0, 2, 2, 1, 1, 1, 0,
-              0, 2, 1, 1, 2, 1, 0],
-    # 'board': [0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0,
-    #           0, 0, 0, 0, 0, 0, 0],
-    'mark': 1
-  }
+  @staticmethod
+  def test(printStates):
+    observation = { 
+      'board': [0, 0, 0, 0, 0, 0, 0,
+                0, 1, 0, 0, 0, 0, 0,
+                0, 2, 0, 0, 0, 0, 0,
+                0, 1, 0, 2, 2, 1, 0,
+                0, 2, 2, 1, 1, 1, 0,
+                0, 2, 1, 1, 2, 1, 0],
+      # 'board': [0, 0, 0, 0, 0, 0, 0,
+      #           0, 0, 0, 0, 0, 0, 0,
+      #           0, 0, 0, 0, 0, 0, 0,
+      #           0, 0, 0, 0, 0, 0, 0,
+      #           0, 0, 0, 0, 0, 0, 0,
+      #           0, 0, 0, 0, 0, 0, 0],
+      'mark': 1
+    }
 
-  configuration = {'timeout': 5, 'columns': 7, 'rows': 6, 'inarow': 4, 'steps': 1000}
-  board = Board(configuration)
-  state = board.assessState(observation)
-  print("  BOARD STATUS:")
-  print(1 * state["myBoard"] + 2 * state["opponentBoard"] + 3 * state["playOptions"])
-  print("  MY SCORE: ", state["score"])
-  if (printStates):
-    print("\nMy state:")
-    print("maximum value: ", state["myState"]["maxValue"])
-    print("vertical:")
-    print(state["myState"]["filtersOutCome"]["vertical"])
-    print("horizontal:")
-    print(state["myState"]["filtersOutCome"]["horizontal"])
-    print("mainDiag: ")
-    print(state["myState"]["filtersOutCome"]["mainDiag"])
-    print("subDiag:")
-    print(state["myState"]["filtersOutCome"]["subDiag"])
+    configuration = {'timeout': 5, 'columns': 7, 'rows': 6, 'inarow': 4, 'steps': 1000}
+    board = Board(configuration)
+    state = board.assessState(observation)
+    print("  BOARD STATUS:")
+    print(1 * state["myBoard"] + 2 * state["opponentBoard"] + 3 * state["playOptions"])
+    print("  MY SCORE: ", state["score"])
+    if (printStates):
+      print("\nMy state:")
+      print("maximum value: ", state["myState"]["maxValue"])
+      print("vertical:")
+      print(state["myState"]["filtersOutCome"]["vertical"])
+      print("horizontal:")
+      print(state["myState"]["filtersOutCome"]["horizontal"])
+      print("mainDiag: ")
+      print(state["myState"]["filtersOutCome"]["mainDiag"])
+      print("subDiag:")
+      print(state["myState"]["filtersOutCome"]["subDiag"])
 
-# testBoard(False)
+# Board.test(False)
