837a1b091f7da5eb9ff4ed0277bf195096c0276f
giladh <gdpstd@gmail.com>
2020-04-28 14:45:42 +0300

    Adds additional dense layer

--- code/deepAgent.py
+++ code/deepAgent.py
@@ -10,25 +10,27 @@ class ScoreModel(nn.Module):
     super(ScoreModel, self).__init__()
     self.config = config
     self.criterion = nn.MSELoss()
-    self.conv1 = nn.Conv2d(3, 32, config["inarow"])  # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0)
-    self.conv2 = nn.Conv2d(32, 64, 3)
-    self.createDenseLayer()
+    self.conv1 = nn.Conv2d(3, 64, config["inarow"])  # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0)
+    self.conv2 = nn.Conv2d(64, 128, 3)
+    self.createDenseLayers()
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     self.optimizer = optim.SGD(self.parameters(), lr=LearningRate)
 
   # calculates the input size of the dense layer:
-  def createDenseLayer(self):
+  def createDenseLayers(self):
     dummyIn = torch.randn(1, 3, self.config["rows"], self.config["columns"])
     dummyIn = nn.functional.relu(self.conv1(dummyIn))
     dummyOut = nn.functional.relu(self.conv2(dummyIn))
     self.denseInputSize = np.prod(dummyOut.shape)
-    self.dense1 = nn.Linear(self.denseInputSize, self.config["columns"], bias="True")
+    self.dense1 = nn.Linear(self.denseInputSize, 4 * self.config["columns"], bias="True")
+    self.dense2 = nn.Linear(4 * self.config["columns"], self.config["columns"], bias="True")
 
   def forward(self, x):
     x = nn.functional.relu(self.conv1(x))
     x = nn.functional.relu(self.conv2(x))
     x = x.view(-1, self.denseInputSize)
-    x = nn.functional.normalize(self.dense1(x))
+    x = nn.functional.relu(self.dense1(x))
+    x = nn.functional.normalize(self.dense2(x))
     return x
 
   def play(self, input):
@@ -51,7 +53,7 @@ class ScoreModel(nn.Module):
     loss = self.criterion(output, GT)
     loss.backward()
     self.optimizer.step()
-    return
+    return torch.Tensor.cpu(loss.data).numpy()
 
   def mutate(self, genNumber, GAMMA):
     modelLayers = [
